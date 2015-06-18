362cb80316142e7523d5ac137952419ec88a15f8
Ilya Hrytsuk <bypotatoes@gmail.com>
2015-06-18 15:04:49 +0300

    Fetch daily kzt rates

--- lib/kazakhstan_national_bank.rb
+++ lib/kazakhstan_national_bank.rb
@@ -1,5 +1,30 @@
-require "kazakhstan_national_bank/version"
+require 'kazakhstan_national_bank/version'
+require 'money'
+require 'open-uri'
+require 'rss'
 
-module KazakhstanNationalBank
-  # Your code goes here...
+class Money
+  module Bank
+    class KazakhstanNationalBank < Money::Bank::VariableExchange
+      SERVICE_HOST = 'www.nationalbank.kz'
+      SERVICE_PATH = '/rss/rates_all.xml'
+
+      def update_rates
+        daily_exchange_rates
+      end
+
+      private
+
+      def uri
+        URI::HTTP.build(host: SERVICE_HOST, path: SERVICE_PATH)
+      end
+
+      def daily_exchange_rates
+        RSS::Parser.parse(uri.read, false).items.
+          each_with_object({'KZT_TO_KZT' => 1.0}) do |rate, rates|
+            rates["KZT_TO_#{rate.title}"] = rate.description.to_f
+          end
+      end
+    end
+  end
 end
--- lib/kazakhstan_national_bank/version.rb
+++ lib/kazakhstan_national_bank/version.rb
@@ -1,3 +1,3 @@
 module KazakhstanNationalBank
-  VERSION = "0.0.1"
+  VERSION = '0.1'
 end
